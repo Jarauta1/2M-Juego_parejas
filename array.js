c3e04f1ca99b4332c08329460a6eab054f759adb
Diego Jarauta <diegojarauta8@gmail.com>
2020-11-13 20:10:18 +0100

    errores solventados

--- array.js
+++ array.js
@@ -1,13 +1,13 @@
 let array = [{
-        imagen: https: //i.pinimg.com/originals/28/49/ca/2849ca8b25477169a6a3db8ce04fd0da.jpg
+        imagen: "https://i.pinimg.com/originals/28/49/ca/2849ca8b25477169a6a3db8ce04fd0da.jpg"
     },
     {
-        imagen: https: //us.123rf.com/450wm/bigbaraboom/bigbaraboom1612/bigbaraboom161200182/68903422-bear-logo-para-club-deportivo-o-equipo-logotipo-de-la-mascota-de-la-mascota-modelo-ilustraci%C3%B3n-vectoria.jpg?ver=6
+        imagen: "https://us.123rf.com/450wm/bigbaraboom/bigbaraboom1612/bigbaraboom161200182/68903422-bear-logo-para-club-deportivo-o-equipo-logotipo-de-la-mascota-de-la-mascota-modelo-ilustraci%C3%B3n-vectoria.jpg?ver=6"
     },
     {
-        imagen: https: //static.skillshare.com/uploads/project/c23292cadf0fe4d9546e2ccd45e88896/18e0a0ce
+        imagen: "https://static.skillshare.com/uploads/project/c23292cadf0fe4d9546e2ccd45e88896/18e0a0ce"
     },
     {
-        imagen: https: //i.pinimg.com/736x/e4/73/5d/e4735d6e2406c0387ff87e54d5ffce6a.jpg
+        imagen: "https://i.pinimg.com/736x/e4/73/5d/e4735d6e2406c0387ff87e54d5ffce6a.jpg"
     }
 ]
